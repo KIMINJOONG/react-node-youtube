3effc03c0aed3f2d43bffaa809613a039202c0d3
KIMINJOONG <ijboym216@gmail.com>
2020-03-16 17:43:11 +0900

    11. login with bcrypt part one

--- models/User.js
+++ models/User.js
@@ -45,8 +45,19 @@ userSchema.pre("save", function(next) {
                 next();
             });
         });
+    } else {
+        next();
     }
 });
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken;
 
 const User = mongoose.model("User", userSchema);
 
