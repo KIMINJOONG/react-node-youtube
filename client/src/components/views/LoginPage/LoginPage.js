b981e062323aa0b7d6c84f0c21922c0636f3b326
KIMINJOONG <ijboym216@gmail.com>
2020-03-24 20:16:59 +0900

    30 login page part two

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-const LoginPage = () => {
+const LoginPage = props => {
+    const dispatch = useDispatch();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -10,6 +14,23 @@ const LoginPage = () => {
 
     const onPasswordHandler = e => {
         setPassword(e.target.value);
+    };
+
+    const onSubmitHandler = async e => {
+        e.preventDefault();
+
+        let body = {
+            email,
+            password
+        };
+
+        dispatch(loginUser(body)).then(response => {
+            if (response.payload.loginSuccess) {
+                props.history.push("/");
+            } else {
+                alert("Error");
+            }
+        });
     };
     return (
         <div
@@ -21,7 +42,10 @@ const LoginPage = () => {
                 height: "100vh"
             }}
         >
-            <form style={{ display: "flex", flexDirection: "column" }}>
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={onSubmitHandler}
+            >
                 <label>Email</label>
                 <input type="email" value={email} onChange={onEmailHandler} />
                 <label>Password</label>
