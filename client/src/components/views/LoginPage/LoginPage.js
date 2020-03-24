2317f1c956b273456398e1fecd335da32ef0cde4
KIMINJOONG <ijboym216@gmail.com>
2020-03-24 20:04:05 +0900

    29 login page part one

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onEmailHandler = e => {
+        setEmail(e.target.value);
+    };
+
+    const onPasswordHandler = e => {
+        setPassword(e.target.value);
+    };
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh"
+            }}
+        >
+            <form style={{ display: "flex", flexDirection: "column" }}>
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={onPasswordHandler}
+                />
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
+    );
 };
 
 export default LoginPage;
