3d04b84b62a7d3eb270427e3f313c56059228c78
KIMINJOONG <ijboym216@gmail.com>
2020-03-26 16:56:20 +0900

    33 auth check part one

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+
+export default function(SpecificComponent, option, adminRoute = null) {
+    // option
+    // null => 아무나 출입이 가능한 페이지
+    // true => 로그인한 유저만 출입이 가능한 페이지
+    // false => 로그인한 유저는 출입 불가능한 페이지
+
+    const AuthenticationCheck = props => {
+        const dispatch = useDispatch();
+        useEffect(() => {
+            dispatch(auth()).then(response => console.log(response));
+        }, []);
+    };
+    return AuthenticationCheck;
+}
