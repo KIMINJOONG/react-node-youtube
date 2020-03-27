55a4e136ffda671407214e95bb60457c26808332
KIMINJOONG <ijboym216@gmail.com>
2020-03-27 16:58:06 +0900

    34 auth check part two

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -11,7 +11,24 @@ export default function(SpecificComponent, option, adminRoute = null) {
     const AuthenticationCheck = props => {
         const dispatch = useDispatch();
         useEffect(() => {
-            dispatch(auth()).then(response => console.log(response));
+            dispatch(auth()).then(response => {
+                console.log(response);
+                if (!response.payload.isAuth) {
+                    // 로그인 하지 않은 상태
+                    if (option) {
+                        props.history.push("/login");
+                    }
+                } else {
+                    // 로그인 한 상태
+                    if (adminRoute && !response.payload.isAdmin) {
+                        props.history.push("/");
+                    } else {
+                        if (option === false) {
+                            props.history.push("/");
+                        }
+                    }
+                }
+            });
         }, []);
     };
     return AuthenticationCheck;
